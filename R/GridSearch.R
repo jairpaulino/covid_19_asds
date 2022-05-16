14046d65b64d4b7f4e5c0423f19452831a62f231
jairpaulino <63611561+jairpaulino@users.noreply.github.com>
2022-05-16 10:07:17 -0300

    Update GridSearch.R
    Update GridSearch.R

--- R/GridSearch.R
+++ R/GridSearch.R
@@ -1,3 +1,5 @@
+epochs = 200
+
 getSVRParameters_GridSearch = function(train, valid, grid){
   # train=train_ts; valid=valid_ts
   #grid = list(lag = c(2, 5, 10, 15, 20)
@@ -33,7 +35,7 @@ getSVRParameters_GridSearch = function(train, valid, grid){
               
               train_valid_df = getSlideWindowMatrix(c(train, valid), svrParameters$lag)
               #train_valid_df[,1:svrParameters$lag] = apply(train_valid_df[,1:svrParameters$lag]
-              #                                             , MARGIN = 2, FUN = getNormalizeTS
+              #                                             , MARGIN = 2, FUN = getNormalizedTS
               #                                             , max = max(train), min = min(train))
               len = length(train_valid_df$target)
               valid_len = length(valid)
@@ -115,9 +117,9 @@ getELMParameters_GridSearch = function(train, valid, grid){
         elmParameters$nhid = grid$nhid[k]
         
         train_valid_df = getSlideWindowMatrix(c(train, valid), elmParameters$lag)
-        train_valid_df[,1:elmParameters$lag] = apply(train_valid_df[,1:elmParameters$lag]
-                                                     , MARGIN = 2, FUN = getNormalizeTS
-                                                     , max = max(train), min = min(train))
+        #train_valid_df[,1:elmParameters$lag] = apply(train_valid_df[,1:elmParameters$lag]
+        #                                             , MARGIN = 2, FUN = getNormalizedTS
+        #                                             , max = max(train), min = min(train))
         
         #View(train_valid_df)
         len = length(train_valid_df$target)
@@ -193,9 +195,9 @@ getLSTMParameters_GridSearch = function(train, valid, grid){
           lstmParameters$nhid = grid$nhid[l]
           
           train_valid_df = getSlideWindowMatrix(c(train, valid), lstmParameters$lag)
-          train_valid_df[,1:(lstmParameters$lag+1)] = apply(train_valid_df[,1:(lstmParameters$lag+1)]
-                                                       , MARGIN = 2, FUN = getNormalizeTS
-                                                       , max = max(train), min = min(train))
+          #train_valid_df[,1:(lstmParameters$lag+1)] = apply(train_valid_df[,1:(lstmParameters$lag+1)]
+          #                                             , MARGIN = 2, FUN = getNormalizedTS
+          #                                             , max = max(train), min = min(train))
           #View(train_valid_df)
           len = length(train_valid_df$target)
           valid_len = length(valid)
@@ -242,14 +244,14 @@ getLSTMParameters_GridSearch = function(train, valid, grid){
             fit(x = covariates_train
                 , y = target_train
                 , callbacks = c(early_stopping)
-                , epochs = 20
+                , epochs = epochs
                 #, shuffle = FALSE
                 , batch_size = 1
-                , validation_split = 0.2
+                , validation_split = 0.25
                 #, validation_data = list(covariates_valid, target_valid)
                 , verbose = TRUE)
           
-          predLSTM = predict(modelLSTM, covariates_valid)
+          predLSTM = predict(modelLSTM, covariates_valid, rep=10)
           MSE = getMSE(target_valid, predLSTM)
           #plot.ts(target_valid); lines(predLSTM, col=2)
           #plot.ts(predLSTM)
@@ -272,6 +274,112 @@ getLSTMParameters_GridSearch = function(train, valid, grid){
       }
     }
   }
+  end = proc.time()[[3]] - begin[[3]]
+  print(paste0("Best parameters: ", list(bestPar)))
+  print(paste0("Processing time: ", end))
+  sink()
+  
+  rtr = list()
+  rtr$bestPar = bestPar
+  rtr$MSE_min = MSE_min
+  rtr$proc_time = end
+  return(rtr)
+}
+
+getMLPParameters_GridSearch = function(train, valid, grid){
+  # train= normTrain; valid=normValid
+  # grid = list(lag = c(14)#2, 10, 15, 20)
+  #             , actfun = c("relu", "sigmoid")
+  #             , nhid = c(10, 25, 50, 100)
+  #             , optim = c('adam'))
+  
+  sink(file = paste("Results/", country, "/" ,"gridSearch_", country, "_MLP_Parameters.txt", sep="")
+       , append = FALSE, type = "output", split = FALSE); 
+  
+  begin = proc.time()
+  
+  MSE_min = 1e10; nModels = 0
+  for(i in 1:length(grid$lag)){
+    for (j in 1:length(grid$optim)){
+      for (k in 1:length(grid$actfun)){
+        for (l in 1:length(grid$nhid)){
+          #i=1; j=1; k=1; l=1
+          
+          nModels = nModels + 1
+          mlpParameters = list()
+          mlpParameters$lag = grid$lag[i]
+          mlpParameters$optim = grid$optim[j]
+          mlpParameters$actfun = grid$actfun[k]
+          mlpParameters$nhid = grid$nhid[l]
+          
+          train_valid_df = getSlideWindowMatrix(c(train, valid), mlpParameters$lag)
+          #train_valid_df[,1:(lstmParameters$lag+1)] = apply(train_valid_df[,1:(lstmParameters$lag+1)]
+          #                                                  , MARGIN = 2, FUN = getNormalizedTS
+          #                                                 , max = max(train), min = min(train))
+          
+          #View(train_valid_df)
+          len = length(train_valid_df$target)
+          valid_len = length(valid)
+          trainNorm_ts = train_valid_df[1:(len-valid_len),]
+          validNorm_ts = train_valid_df[(len-valid_len+1):len,]
+          #length(valid); length(validNorm_ts$target)
+          #plot.ts(valid); lines(validNorm_ts$target, col=2)
+          #length(train); length(trainNorm_ts$target)
+          
+          covariates_train = as.matrix(trainNorm_ts[,1:mlpParameters$lag])
+          train_target = trainNorm_ts$target
+          covariates_valid = as.matrix(validNorm_ts[,1:mlpParameters$lag])
+          valid_target = validNorm_ts$target
+          
+          modelMLP = keras_model_sequential()
+          modelMLP %>%
+            layer_dense(units = mlpParameters$nhid
+                        , input_shape = c(mlpParameters$lag) 
+                        , activation = mlpParameters$actfun) %>%
+            layer_dense(units = 1, activation = 'linear')
+          
+          modelMLP %>% 
+            compile(loss = 'mse'
+                    , optimizer = mlpParameters$optim
+                    , metrics = 'mse')
+          
+          early_stopping = callback_early_stopping(monitor = 'val_loss'
+                                                   , patience = 5)
+          
+          history = modelMLP %>% 
+            fit(x = covariates_train
+                , y = train_target
+                , callbacks = c(early_stopping)
+                , epochs = epochs
+                , shuffle = FALSE
+                , batch_size = 1
+                , validation_split = 0.25
+                , verbose = TRUE)
+             
+          predMLP = predict(modelMLP, covariates_valid, rep=10)
+          MSE = getMSE(valid_target, predMLP)
+          
+          #plot.ts(target_valid); lines(predMLP, col=2)
+
+          print(paste0("Iter.: ", nModels ," | lag: ", mlpParameters$lag
+                       , " | optim: ", mlpParameters$optim
+                       , " | actfun: ",  mlpParameters$actfun
+                       , " | nhid: ",  mlpParameters$nhid
+                       , " | MSE: ", round(MSE, 5)));
+          if(is.na(MSE)){
+            print(paste0("NA value in ", nModels, " iteration."))
+          }else{
+            if(MSE < MSE_min){
+              print(paste0(round(MSE_min, 5), " --> ", round(MSE, 5), " - it: ", nModels))
+              MSE_min = MSE
+              bestPar = mlpParameters
+            }
+          }
+        }
+      }
+    }
+  }
+  
   end = proc.time()[[3]] - begin[[3]]
   print(paste0("Best parameters: ", list(bestPar)))
   print(paste0("Processing time: ", end))
